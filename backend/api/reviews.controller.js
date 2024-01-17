8f37334abdfe3438b2aa25bf0ae33cf7fb863f7d
demoreon <demoreon@gmail.com>
2024-01-18 00:21:36 +0100

    Add files via upload

---
+++ backend/api/reviews.controller.js
@@ -0,0 +1,95 @@
+import ReviewsDAO from "../dao/reviewsDAO.js"
+
+export default class ReviewsController {
+  static async apiPostReview(req, res, next) {
+    try {
+      const movieId = parseInt(req.body.movieId)
+      const review = req.body.review
+      const user = req.body.user
+
+      const reviewResponse = await ReviewsDAO.addReview(
+        movieId,
+        user,
+        review
+      )
+      res.json({ status: "success" })
+    } catch (e) {
+      res.status(500).json({ error: e.message })
+    }
+  }
+
+
+  static async apiGetReview(req, res, next) {
+    try {
+      let id = req.params.id || {}
+      let review = await ReviewsDAO.getReview(id)
+      if (!review) {
+        0
+      }
+      res.json(review)
+   } catch (e) {
+      console.log(`api, ${e}`)
+      res.status(500).json({ error: e })
+    }
+  }
+
+  static async apiUpdateReview(req, res, next) {
+    try {
+      const reviewId = req.params.id
+      const review = req.body.review
+      const user = req.body.user
+
+      const reviewResponse = await ReviewsDAO.updateReview(
+        reviewId,
+        user,
+        review
+      )
+
+      var { error } = reviewResponse
+      if (error) {
+        res.status(400).json({ error })
+      }
+
+      if (reviewResponse.modifiedCount === 0) {
+        throw new Error(
+          "unable to update review",
+        )
+      }
+
+      res.json({ status: "success" })
+  } catch (e) {
+      res.status(500).json({ error: e.message })
+  }
+}
+
+  static async apiDeleteReview(req, res, next) {
+    try {
+      const reviewId = req.params.id;
+      const reviewResponse = await ReviewsDAO.deleteReview(reviewId);
+
+      if (reviewResponse.deletedCount === 1) {
+        res.json({ status: "success" });
+      } else {
+        res.status(404).json({ error: "Review not fojund" });
+      }
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  }
+
+
+  static async apiGetReviews(req, res, next) {
+    try {
+      let id = req.params.id || {}
+      let reviews = await ReviewsDAO.getReviewsByMovieId(id)
+      if (!reviews) {
+        res.status(404).json({ error: "Movie Id Not found" })
+        return
+      }
+      res.json(reviews)
+    } catch (e) {
+      console.log(`api, ${e}`)
+      res.status(500).json({ error: e })
+    }
+  }
+}
